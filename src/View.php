3d8847b8afa143258c2a3b335c5f7f79b62396f9
Alex Pinkevych <pinchoalex@gmail.com>
2019-09-21 14:38:54 +0300

    New param "exclude" preg_match pattern

--- src/View.php
+++ src/View.php
@@ -14,6 +14,7 @@ class View extends \yii\web\View
      * @var bool
      */
     public $compress = true;
+    public $exclude = null;
 
     /**
      * @inheritdoc
@@ -24,13 +25,23 @@ class View extends \yii\web\View
 
         if ($this->compress === true) {
             \Yii::$app->response->on(\yii\web\Response::EVENT_BEFORE_SEND, function (\yii\base\Event $Event) {
-                $Response = $Event->sender;
-                if ($Response->format === \yii\web\Response::FORMAT_HTML) {
-                    if (!empty($Response->data)) {
-                        $Response->data = self::compress($Response->data);
-                    }
-                    if (!empty($Response->content)) {
-                        $Response->content = self::compress($Response->content);
+                if (!empty($this->exclude)) {
+                    preg_match(
+                        $this->exclude, // '/\/load\/0-0-0-(.*?)-13/'
+                        \Yii::$app->request->url,
+                        $matches,
+                        PREG_OFFSET_CAPTURE
+                    );
+                }
+                if (!isset($matches[1]) || empty($matches[1])) {
+                    $Response = $Event->sender;
+                    if ($Response->format === \yii\web\Response::FORMAT_HTML) {
+                        if (!empty($Response->data)) {
+                            $Response->data = self::compress($Response->data);
+                        }
+                        if (!empty($Response->content)) {
+                            $Response->content = self::compress($Response->content);
+                        }
                     }
                 }
             });
@@ -45,7 +56,7 @@ class View extends \yii\web\View
      */
     public static function compress($html)
     {
-        $filters = array(
+        $filters = [
             // remove javascript comments
             '/(?:<script[^>]*>|\G(?!\A))(?:[^\'"\/<]+|"(?:[^\\"]+|\\.)*"|\'(?:[^\\\']+|\\.)*\'|\/(?!\/)|<(?!\/script))*+\K\/\/[^\n|<]*/xsu' => '',
             // remove html comments except IE conditions
@@ -58,7 +69,7 @@ class View extends \yii\web\View
             '/\s{2,}/u' => ' ',
             // collapse new lines
             '/(\r?\n)/u' => '',
-        );
+        ];
 
         $output = preg_replace(array_keys($filters), array_values($filters), $html);
 
